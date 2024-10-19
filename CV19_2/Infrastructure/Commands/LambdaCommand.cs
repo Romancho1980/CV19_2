59adad47d657dfdf5fc7e820b7f01ef5795f2669
Roman Scherbakow <rip19880@gmail.com>
2024-10-19 22:49:20 +0300

    Главный класс Комманды для приложения

---
+++ CV19_2/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,27 @@
+﻿using CV19_2.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV19_2.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool> _canExecute;
+
+        public LambdaCommand(Action<object?> Execute,Func<object?,bool> CanExecute)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+        public override bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object? parameter)=> _execute(parameter);
+    }
+}
